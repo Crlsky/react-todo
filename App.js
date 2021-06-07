8264b18f7168409432a8c3dc03e7bab47430ecd0
Karol <karol.swieciochowski@gmail.com>
2021-06-07 15:22:49 +0200

    add todo button

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import Form from './components/todoForm';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffe", date: "24.08.2020", key: '1'},
+    { text: "buy bread", date: "24.08.2020", key: '2'},
+    { text: "buy bananas", date: "24.08.2020", key: '3'}
+  ]);
+
+  const delItem = (key) => {
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
+
+  const addItem = (text, date) => {
+    setTodos((prevTodos)=>{
+      return [
+        {text: text, date: date, key: Math.random().toString()},
+        ...prevTodos
+      ]
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <Form addItem={addItem}/>
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({item})=>(
+              <TodoItem item={item} delItem={delItem}/>
+            )}
+          />
+        </View>
+      </View>
+      
     </View>
   );
 }
@@ -18,4 +52,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  content: {
+    paddingTop: 20,
+  },
+
+  list: {
+    marginTop: 20,
+  }
 });
